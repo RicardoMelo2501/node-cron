c8c48fe847373c746e938bb0a05576290ad96c8e
Ricardo Melo <83253741+RicardoMelo2501@users.noreply.github.com>
2023-12-21 00:00:08 -0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,8 @@ const { exec } = require('child_process');
 
 cron.schedule(`*/1 * * * *`, async () => {
   // Execute o script PHP usando o comando PHP
-  exec('php seu_script.php', (error, stdout, stderr) => {
-    if (error) {
+  exec('php seu_script.php', (error: unknown, stdout: string, stderr: string) => {
+     if (error !== null && error !== undefined) {
       console.error(`Erro ao executar o script PHP: ${error}`);
       return;
     }
