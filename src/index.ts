1ee07913bfd11d30f99504d6f2797314ad78e1c6
Ricardo Melo <83253741+RicardoMelo2501@users.noreply.github.com>
2023-12-20 23:56:47 -0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,9 @@
-import cron from 'node-cron';
+const cron = require('node-cron');
 const { exec } = require('child_process');
 
 cron.schedule(`*/1 * * * *`, async () => {
-  console.log(`running your task...`);
-
-    // Substitua 'seu_script.php' pelo caminho do seu script PHP
-  const comandoPHP = 'php teste.php';
-
   // Execute o script PHP usando o comando PHP
-  exec(comandoPHP, (error, stdout, stderr) => {
+  exec('php seu_script.php', (error, stdout, stderr) => {
     if (error) {
       console.error(`Erro ao executar o script PHP: ${error}`);
       return;
